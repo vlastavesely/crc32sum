5d758b2d5dffb85436bf5d8c68965a1c1746ff4b
Vlasta Vesely <vlastavesely@protonmail.ch>
2018-03-04 10:10:26 +0100

    added crc32_file()

--- crc32.c
+++ crc32.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-
+#define BUFSIZE 4096
 
 static unsigned int crc32(unsigned char *data, unsigned int nbytes,
 			  unsigned int crc)
@@ -21,6 +23,23 @@ static unsigned int crc32(unsigned char *data, unsigned int nbytes,
 	}
 
 	return ~crc;
+}
+
+static unsigned int crc32_file(const char *filename)
+{
+	unsigned int n, crc = 0;
+	unsigned char buffer[BUFSIZE];
+	int fd = open(filename, O_RDONLY);
+
+	if (fd < 0)
+		return 0;
+
+	while ((n = read(fd, buffer, sizeof(buffer))) > 0)
+		crc = crc32(buffer, n, crc);
+
+	close(fd);
+
+	return crc;
 }
 
 static void print_crc32(unsigned int crc)
@@ -43,6 +62,10 @@ int main(int argc, const char **argv)
 
 	crc = crc32("he", 2, 0);
 	crc = crc32("llo", 3, crc);
+	print_crc32(crc);
+	puts("--------------------");
+
+	crc = crc32_file("zeroes");
 	print_crc32(crc);
 
 	return 0;
