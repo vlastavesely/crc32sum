862e069cb28704d69066f2696208a827eaab182b
Vlasta Vesely <vlastavesely@protonmail.ch>
2018-03-04 14:01:03 +0100

    added options parser

--- crc32.c
+++ crc32.c
@@ -3,8 +3,30 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <getopt.h>
 
+#define PROGNAME "crc32"
+#define VERSION "0.1"
 #define BUFSIZE 4096
+
+static const char *short_opts = "hvc:";
+
+static const struct option long_opts[] = {
+	{"help",       no_argument,        0, 'h'},
+	{"version",    no_argument,        0, 'v'},
+	{"check",      required_argument,  0, 'c'},
+	{0, 0, 0, 0}
+};
+
+static void show_usage()
+{
+	puts("*usage*"); /* TODO */
+}
+
+static void show_version()
+{
+	puts(PROGNAME " v" VERSION);
+}
 
 static unsigned int crc32(unsigned char *data, unsigned int nbytes,
 			  unsigned int crc)
@@ -47,26 +69,60 @@ static void print_crc32(unsigned int crc)
 	printf("%08x\n", crc);
 }
 
-
-int main(int argc, const char **argv)
+int do_file_checksum(const char *filename)
 {
-	unsigned int crc;
-
-	crc = crc32("hello", 5, 0);
-	print_crc32(crc);
-	puts("--------------------");
-
-	crc = crc32("world", 5, 0);
-	print_crc32(crc);
-	puts("--------------------");
-
-	crc = crc32("he", 2, 0);
-	crc = crc32("llo", 3, crc);
-	print_crc32(crc);
-	puts("--------------------");
-
-	crc = crc32_file("zeroes");
-	print_crc32(crc);
-
+	printf("file: %s\n", filename);
+	/* TODO */
 	return 0;
+}
+
+int do_check(const char *filename)
+{
+	/* TODO */
+}
+
+int main(int argc, char *const *argv)
+{
+	int c = 0, retval = 0;
+	int opt_index = 0;
+	const unsigned char *check = NULL;
+
+	while (c != -1) {
+		c = getopt_long(argc, argv, short_opts, long_opts, &opt_index);
+
+		switch (c) {
+		case 'v':
+			show_version();
+			retval = 0;
+			goto out;
+		case 'h':
+			show_usage();
+			retval = 0;
+			goto out;
+		case 'c':
+			check = optarg;
+			break;
+		case 0:
+		case '?':
+			show_usage();
+			retval = 1;
+			goto out;
+		default:
+			break;
+		}
+	}
+
+	if (check) {
+		retval = do_check(check);
+		goto out;
+	}
+
+	if (optind < argc) {
+		while (optind < argc)
+			if (do_file_checksum(argv[optind++]) != 0)
+				retval++;
+	}
+
+out:
+	return retval;
 }
