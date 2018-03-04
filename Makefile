d6cd42e741ed854715f61a2c5e466c0f31672e26
Vlasta Vesely <vlastavesely@protonmail.ch>
2018-03-04 17:45:26 +0100

    added test.sh

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
-.PHONY: all clean
+.PHONY: all test clean
 
 all:
-	$(CC) crc32.c -o crc32
-	./crc32
+	$(CC) crc32.c -o crc32 -Ofast
+
+test:
+	sh test.sh
 
 clean:
 	$(RM) -f crc32
---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+test -f crc32 || make
+
+echo -n "hello" >hello
+echo -n "world" >world
+dd if=/dev/zero of=zeroes bs=1024 count=7 2>/dev/null
+dd if=/dev/zero of=big bs=8192 count=8192 2>/dev/null
+
+./crc32 hello world zeroes big >check
+
+content=$(cat check)
+if ! test x"$content" = x"3610a686  hello
+3a771143  world
+309137d4  zeroes
+b2eb30ed  big"; then
+	echo "\033[31merror: checksum generation failed.\033[0m"
+	exit 1
+fi
+
+output=$(./crc32 -c check)
+if ! test x"$output" = x"hello: OK
+world: OK
+zeroes: OK
+big: OK"; then
+	echo "\033[31merror: checksum verification failed.\033[0m"
+	exit 1
+fi
+
+rm hello world zeroes big check
+
+echo "\033[32msuccess\033[0m"
