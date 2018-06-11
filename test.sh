dfce56bd846fbe1001d60fe53dc78e803fe333d5
Vlasta Vesely <vlastavesely@protonmail.ch>
2018-06-11 18:09:14 +0200

    test.sh: add test for '-r' argument

--- test.sh
+++ test.sh
@@ -5,28 +5,44 @@ binary=crc32sum
 
 test -f $binary || make
 
-echo -n "hello" >hello
-echo -n "world" >world
-dd if=/dev/zero of=zeroes bs=1024 count=7 2>/dev/null
-dd if=/dev/zero of=big bs=8192 count=8192 2>/dev/null
+mkdir -p ./test
 
-./$binary hello world zeroes big >check
+echo -n "hello" >./test/hello
+echo -n "world" >./test/world
+dd if=/dev/zero of=./test/zeroes bs=1024 count=7 2>/dev/null
+dd if=/dev/zero of=./test/big bs=8192 count=8192 2>/dev/null
+
+./$binary ./test/hello ./test/world ./test/zeroes ./test/big >check
 
 content=$(cat check)
-if ! test x"$content" = x"3610a686  hello
-3a771143  world
-309137d4  zeroes
-b2eb30ed  big"; then
+if ! test x"$content" = x"\
+3610a686  ./test/hello
+3a771143  ./test/world
+309137d4  ./test/zeroes
+b2eb30ed  ./test/big"; then
 	echo "\033[31merror: checksum generation failed.\033[0m"
 	exit 1
 fi
 
 output=$(./$binary -c check)
-if ! test x"$output" = x"hello: OK
-world: OK
-zeroes: OK
-big: OK"; then
+if ! test x"$output" = x"\
+./test/hello: OK
+./test/world: OK
+./test/zeroes: OK
+./test/big: OK"; then
 	echo "\033[31merror: checksum verification failed.\033[0m"
+	exit 1
+fi
+
+output=$(./$binary -r test)
+
+# The files may not be ordered.
+if test -z "$(echo "$output" | grep 'b2eb30ed  test/big' | cat)" ||	\
+   test -z "$(echo "$output" | grep '3610a686  test/hello' | cat)" ||	\
+   test -z "$(echo "$output" | grep '3a771143  test/world' | cat)" ||	\
+   test -z "$(echo "$output" | grep '309137d4  test/zeroes' | cat)"
+then
+	echo "\033[31merror: argument '--recursive' not working.\033[0m"
 	exit 1
 fi
 
@@ -42,6 +58,7 @@ if ! test x"$sum" = x"3610a686"; then
 	exit 1
 fi
 
-rm hello world zeroes big check
+rm -f ./check
+rm -rf ./test
 
 echo "\033[32msuccess\033[0m"
